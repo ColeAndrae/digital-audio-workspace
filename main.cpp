c84a96727259e3df1a446d211eb32564dc7e9c30
ColeAndrae <cole@theandraes.com>
2026-02-12 14:07:06 -0800

    Added functional piano roll

--- main.cpp
+++ main.cpp
@@ -4,7 +4,50 @@
 
 double PI = 3.1415926535;
 
+const int TEMPO = 44100;
+const int VOLUME = 5000;
+
 sf::RenderWindow window(sf::VideoMode({1200, 720}), "digital audio workspace");
+
+std::vector<sf::SoundBuffer> buffers(36);
+
+std::vector<sf::Sound> players;
+
+std::vector<float> frequencies = {
+    1661.2, 1568.0, 1480.0, 1397.0, 1318.6, 1244.6, 1174.6, 1108.8, 1046.6,
+    987.8,  932.4,  880.0,  830.6,  766.0,  740.0,  698.5,  659.3,  622.3,
+    587.3,  554.4,  523.3,  493.9,  466.2,  440.0,  415.3,  392.0,  370.0,
+    349.25, 329.65, 311.15, 293.65, 277.2,  261.65, 246.95, 233.1,  220.0};
+
+void playNotes(std::vector<std::vector<bool>> notes) {
+
+  std::vector<std::vector<int16_t>> sounds(36, std::vector<int16_t>());
+
+  for (float r = 0; r < 60; r++) {
+    for (float c = 0; c < 36; c++) {
+      if (notes[r][c]) {
+        for (int i = 0; i < TEMPO; i++) {
+          sounds[c].push_back(VOLUME * sin(PI * frequencies[c] * i / 44100));
+        }
+      } else {
+        for (int i = 0; i < TEMPO; i++) {
+          sounds[c].push_back(0);
+        }
+      }
+    }
+  }
+
+  for (float c = 0; c < 36; c++) {
+    (void)buffers[c].loadFromSamples(
+        sounds[c].data(), sounds[c].size(), 2, 44100,
+        {sf::SoundChannel::FrontLeft, sf::SoundChannel::FrontRight});
+    players.push_back(sf::Sound(buffers[c]));
+  }
+
+  for (float c = 0; c < 36; c++) {
+    players[c].play();
+  }
+}
 
 void drawGrid(std::vector<std::vector<bool>> notes) {
   sf::RectangleShape rectangle({1200, 1});
@@ -42,7 +85,9 @@ int main() {
         window.close();
       }
       if (const auto *keyPressed = event->getIf<sf::Event::KeyPressed>()) {
-        // Play sounds
+        if (keyPressed->code == sf::Keyboard::Key::Space) {
+          playNotes(notes);
+        }
       }
       if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
         sf::Vector2i mousePos = sf::Mouse::getPosition(window);
